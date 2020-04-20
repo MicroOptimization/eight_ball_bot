c109ea8d1d351f17f833af229028c0e7cb9f256c
MicroOptimization <jackiezhen538@gmail.com>
2020-04-20 12:46:12 -0400

    fixed shortcuts

--- eight_ball/src/bot.py
+++ eight_ball/src/bot.py
@@ -22,40 +22,19 @@ class MyClient(discord.Client):
             
         await client.change_presence(activity=discord.Game(name="with Fate"))
         
-        """
-        channel = client.get_channel(701760072560148519)
-        await channel.send("test")
-        """
-        
     async def on_message(self, message):    
         print('Message from {0.author} from {0.channel}: {0.content}'.format(message))
         text = message.content
         words = text.split(" ")
-        """
-        if len(words) == 1:
-            words[0].strip()
-        """ 
-        #print("Words[0]: ". words[0])
-        #print("Words: " , words)
-        """
-        if type(words) == str:
-            print("It's a string")
-            if words == "qqox,".lower():
-                await message.channel.send("Hello, qqox is here")
-        elif type(words) == list:
-        """
-        #print("Words: " , words)
+
         if len(words) >= 2 and words[1] == "help":
             await message.channel.send("To talk to me, begin with 'qqox, <message>'")
         elif words[0] == "qqox,".lower():
-            await message.author.send('Qqox is dead. Rip qqox.')
-        elif len(words) >= 3 and words[0].lower() == "magic" and words[1].lower() == "eight" and words[2].lower() == "ball,":
+            await message.author.send('Qqox is dead.')
+        elif words[0] == "meb," or words[0] == "mge," or (len(words) >= 3 and words[0].lower() == "magic" and words[1].lower() == "eight" and words[2].lower() == "ball,"):
             await message.channel.send("The Magic Eight Ball has Spoken:")
             await message.channel.send("'{}'".format(functions.get_message()))
-        
-        #
-        #await message.author.send("Spaghetti")
-        
+
 client = MyClient()
 
 key = key_retriever.get_key() #Insert your key here
